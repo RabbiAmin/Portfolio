895e61a7e991f0e0d083d241f6f640a7ac11e1e0
rabbiamin <rabbiamin98@gmail.com>
2024-02-25 10:27:18 +0600

    LoveFunnyAdd

---
+++ pages/7_Fun.py
@@ -0,0 +1,10 @@
+
+
+
+import streamlit as st
+
+# GitHub HTML link
+github_html_link = "https://love-fun.vercel.app/"
+
+# Embed HTML content
+st.components.v1.html("<iframe src='{}' width='700' height='500' style='border:none;'></iframe>".format(github_html_link), width=700, height=500)
