1a97fd980a4ff23ff92b37681477f1cc30910bd7
Md. Rabbi Amin <36773584+RabbiAmin@users.noreply.github.com>
2024-11-27 10:36:04 +0000

    qqq

--- constant.py
+++ constant.py
@@ -1,23 +1,13 @@
 info = {
-   "Pronoun": "his", 
-   "Name": "MR Amin",
-   "Full_Name": "Md. Rabbi Amin",
-   "Intro": "Data Analyst (BSc in CSE, MSc in Data Science)",
-   "Tableau":"",
-   "About":"As a graduate of **North South University** with a "
-           "degree in Computer Science, I'm currently deepening my knowledge in **Data Science** through a "
-           "Master's program at **Jahangirnagar University** while also working as a **data analyst** at **Monico Technology**. "
-           "My post-graduation journey involved valuable experiences in internships and roles at two different companies, "
-           "shaping my career in **data science** and **AI**. This role as a data analyst at Monico Technology has "
-           "significantly enhanced my expertise in this field. I've successfully completed various projects, ranging from "
-           "creating a health survey dashboard with Python libraries to delving into graph representation learning with "
-           "SVM, and developing an augmented reality mobile application for image recognition. Proficient in languages like "
-           "**Python, R, and MongoDB**, along with skills in front-end development and version control tools, I am currently "
-           "collaborating with my team on data **analytics** and **dashboard** solutions, contributing to our collective goals.",
-   "Medium":"https://medium.com/@Rabbiamin",
-   "City":"Dhaka, Bangladesh",
-   "Photo":"""<a href=\"https://bd.linkedin.com/in/md-rabbi-amin-a1807b118\"><img src=\"https://media.licdn.com/dms/image/C5103AQFXVP1x-LRUjA/profile-displayphoto-shrink_800_800/0/1561266339921?e=2147483647&v=beta&t=TWer23odbpZA83BK7RWGyH1J2IdMSzu7ChD7KkrbooU\" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
-   "Email": "rabbiamin98@gmail.com"
+    "Name": "MR Amin",
+    "Pronoun": "he",
+    "Full_Name": "Md. Rabbi Amin",
+    "Intro": "Data Analyst (BSc in CSE, MSc in Data Science)",
+    "About": "I am a data scientist with a passion for building cool stuff.",
+    "Medium": "https://medium.com/@Rabbiamin",
+    "City":"Dhaka, Bangladesh",
+    "Photo":"""<a href=\"https://bd.linkedin.com/in/md-rabbi-amin-a1807b118\"><img src=\"https://media.licdn.com/dms/image/C5103AQFXVP1x-LRUjA/profile-displayphoto-shrink_800_800/0/1561266339921?e=2147483647&v=beta&t=TWer23odbpZA83BK7RWGyH1J2IdMSzu7ChD7KkrbooU\" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+    "Email": "rabbiamin98@gmail.com",
 }
 
 
